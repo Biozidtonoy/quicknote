1c9e772ae5088eda795ff9afdd0ddfb62fe8ec29
Biozid Bhuiyan Tonoy <biozidbhuiyantonoy@gmail.com>
2026-07-02 22:36:29 +0600

    create authorization header

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -3,6 +3,8 @@ from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta, UTC
 
+from jose import JWTError
+
 from app.core.config import (
     SECRET_KEY,
     ALGORITHM,
@@ -49,3 +51,64 @@ def create_access_token(data: dict):
     )
 
     return encoded_jwt
+
+
+
+def verify_access_token(token: str):
+
+    try:
+
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        email = payload.get("sub")
+
+        if email is None:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid token"
+            )
+
+        return email
+
+    except JWTError:
+
+        raise HTTPException(
+            status_code=401,
+            detail="Could not validate credentials"
+        )
+    
+def get_current_user(
+
+    token: str = Depends(oauth2_scheme),
+
+    db: Session = Depends(get_db)
+
+):
+
+    email = verify_access_token(token)
+
+    user = (
+
+        db.query(User)
+
+        .filter(User.email == email)
+
+        .first()
+
+    )
+
+    if user is None:
+
+        raise HTTPException(
+
+            status_code=401,
+
+            detail="User not found"
+
+        )
+
+    return user
