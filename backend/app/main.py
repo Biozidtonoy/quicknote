6dd0314450a2c6eae269147665873d5c9b98954f
Biozid Bhuiyan Tonoy <biozidbhuiyantonoy@gmail.com>
2026-07-06 18:04:58 +0600

    add middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import text
 
 from app.database import Base
@@ -14,12 +15,30 @@ from app.api.auth import router as auth_router
 from app.api.users import router as users_router
 from app.api.notes import router as notes_router
 
-
-
-
 from fastapi.staticfiles import StaticFiles
 
+
+
 app = FastAPI()
+
+app.add_middleware(
+
+    CORSMiddleware,
+
+    allow_origins=[
+
+        "http://localhost:5173"
+
+    ],
+
+    allow_credentials=True,
+
+    allow_methods=["*"],
+
+    allow_headers=["*"]
+
+)
+
 app.include_router(auth_router)
 app.include_router(users_router)
 app.include_router(notes_router)
